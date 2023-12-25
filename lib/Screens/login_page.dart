746bc72aaaa02c2d636d5a567dca27ec1e26c656
saadkhawar <saadkhawar289@gmail.com>
2023-12-25 20:14:05 +0500

    jjj   hhhh

--- lib/Screens/login_page.dart
+++ lib/Screens/login_page.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 import 'package:loading_overlay/loading_overlay.dart';
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:vedio_appp/Screens/signup.dart';
 
