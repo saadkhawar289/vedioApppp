4b2861d066f3277b654d78b750cef78c98736d52
saadkhawar <saadkhawar289@gmail.com>
2023-12-26 00:58:58 +0500

    sss

--- lib/Screens/splash.dart
+++ lib/Screens/splash.dart
@@ -9,6 +9,9 @@ import 'login_page.dart';
 class splash_screen extends StatelessWidget {
   const splash_screen({super.key});
 
+
+
+
   @override
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
